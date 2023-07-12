23c07e6b62c3dd35af1aecb8be6482199dffc198
chrishamillsnyk <88782898+chrishamillsnyk@users.noreply.github.com>
2023-07-12 20:08:39 +0100

    Add files via upload

--- terraform.tf
+++ terraform.tf
@@ -20,3 +20,11 @@ terraform {
   required_version = "~> 1.2.0"
 }
 
+resource "aws_security_group_rule" "ssh" {
+  type              = "ingress"
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp"
+  cidr_blocks       = [var.remote_user_addr]
+  security_group_id = aws_security_group.allow.id
+}
